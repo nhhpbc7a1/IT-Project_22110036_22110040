4b9448b130e48c9059c52ff50a3d5f19d6895ec3
Nguyễn Hoàng Huy <114816906+nhhpbc7a1@users.noreply.github.com>
2024-12-08 15:31:59 +0700

    add checkout_page

--- routes/buyer_info.route.js
+++ routes/buyer_info.route.js
@@ -3,7 +3,9 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', async function(req, res) {
-    res.render('vwBuyer/checkout');
+    res.render('vwBuyer_info/profile', {
+        layout: 'buyer_info'
+    });
 });
 
 export default router;
