e37a893f059da63b70affaae582994938daa08c9
Nguyễn Hoàng Huy <114816906+nhhpbc7a1@users.noreply.github.com>
2024-12-09 15:43:50 +0700

    add admin_layout

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import express from  'express';
+import express from 'express';
 import numeral from 'numeral';
 import { engine } from 'express-handlebars';
 import hbs_sections from 'express-handlebars-sections';
@@ -15,16 +15,36 @@ app.engine('hbs', engine({
     extname: 'hbs',
     defaultLayout: 'buyer',
     helpers: {
-        format_number(value){
-            return numeral(value).format('0,0') + ' đ';
+        format_price(value) {
+            return numeral(value).format('0.000') + ' VNĐ';
+        },
+        json(context) {
+            return JSON.stringify(context);
+        },
+        ifEquals(arg1, arg2, options) {
+            return arg1 == arg2 ? options.fn(this) : options.inverse(this);
+        },
+        eq(arg1, arg2) {
+            return arg1 === arg2; // Trả về true nếu 2 giá trị bằng nhau
         },
         section: hbs_sections(),
+        containTopping(array, topping_id) {
+            if (Array.isArray(array)) {
+                return array.some(item => item.topping_id.toString() === topping_id.toString());
+            }
+            return false;
+        },
+        formatDate(date, format) {
+            return moment(date).format(format);  // Định dạng ngày theo format
+        }
     }
 }));
 app.set('view engine', 'hbs');
 app.set('views', './views');
 
-app.use('/public',express.static('public'));
+app.use('/public', express.static('public'));
+
+app.use('/node_modules', express.static('node_modules'));
 
 // app.use(async function(req, res, next) {
 //     const categories = await categoryService.findAll();
@@ -52,7 +72,11 @@ app.use('/checkout', checkoutRouter);
 import buyer_infoRouter from './routes/buyer_info.route.js';
 app.use('/buyer_info', buyer_infoRouter);
 
-app.listen(3000, function() {
+import adminRouter from './routes/admin.route.js';
+app.use('/admin', adminRouter);
+
+
+app.listen(3000, function () {
     console.log('app is running at http://localhost:3000');
 })
 
